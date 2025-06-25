af3a7e9c6fe16c7c6de3b7856b1a3d4a7ff89186
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-06-25 17:23:44 -0300

    feat: creating entity my class

--- src/my-class/entities/my-class.entity.ts
+++ src/my-class/entities/my-class.entity.ts
@@ -1,1 +1,18 @@
-export class MyClass {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type CatDocument = HydratedDocument<MyClass>;
+
+@Schema({ timestamps: true })
+export class MyClass {
+  @Prop({ type: Number, required: true, unique: true })
+  id: number;
+
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  role: string; // Renomeado para evitar uso de palavra reservada
+}
+
+export const MyClassSchema = SchemaFactory.createForClass(MyClass);
