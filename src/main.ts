072012143f4abfdef1d9935f56606e1e63d85971
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-07-14 22:19:23 -0300

    feat: adding swagger to document the API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,22 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('VISO-B.A.S.S.')
+    .setDescription(
+      'VISO-Based API for Structured Storage - API com foco em armazenamento estruturado, baseada no modelo VISO',
+    )
+    .setVersion('1.0')
+    .addTag('interaction')
+    .addTag('object')
+    .addTag('class')
+    .addTag('environment')
+    .addTag('pagerank-friendship')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
