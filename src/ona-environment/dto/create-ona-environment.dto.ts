3422d876e10fca68ab6ebb06de34aa5cd8c3f027
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-07-14 22:35:56 -0300

    feat: create ona environment dto

--- src/ona-environment/dto/create-ona-environment.dto.ts
+++ src/ona-environment/dto/create-ona-environment.dto.ts
@@ -1,1 +1,20 @@
-export class CreateOnaEnvironmentDto {}
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateOnaEnvironmentDto {
+  @IsNotEmpty()
+  @IsString()
+  env_object_i: string;
+
+  @IsNotEmpty()
+  env_total_Intera: number;
+
+  @IsNotEmpty()
+  env_total_valida: number;
+
+  @IsNotEmpty()
+  env_total_new: number;
+
+  @IsNotEmpty()
+  @IsArray()
+  env_adjacency: number[][];
+}
