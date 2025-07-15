5a56327bc6a73537c75228c7898c05d81ee9a11e
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-07-14 22:32:48 -0300

    feat: importing ona environment  schema

--- src/ona-environment/ona-environment.module.ts
+++ src/ona-environment/ona-environment.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { OnaEnvironmentService } from './ona-environment.service';
 import { OnaEnvironmentController } from './ona-environment.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  OnaEnvironment,
+  OnaEnvironmentSchema,
+} from './schema/ona-enviroment.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: OnaEnvironment.name, schema: OnaEnvironmentSchema },
+    ]),
+  ],
   controllers: [OnaEnvironmentController],
   providers: [OnaEnvironmentService],
 })
