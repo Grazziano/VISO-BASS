3b20f5d279ba8692d65bf00e8ca326eda7262faf
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-07-27 15:02:35 -0300

    feat: importing the interactions model

--- src/interaction/interaction.module.ts
+++ src/interaction/interaction.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { InteractionService } from './interaction.service';
 import { InteractionController } from './interaction.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Interaction, InteractionSchema } from './schema/interaction.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Interaction.name, schema: InteractionSchema },
+    ]),
+  ],
   controllers: [InteractionController],
   providers: [InteractionService],
 })
