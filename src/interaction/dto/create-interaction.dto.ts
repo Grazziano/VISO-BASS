040b6352ce7410091834724757d6216f426036a2
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-07-27 15:10:22 -0300

    feat: creating dto interactions

--- src/interaction/dto/create-interaction.dto.ts
+++ src/interaction/dto/create-interaction.dto.ts
@@ -1,1 +1,40 @@
-export class CreateInteractionDto {}
+import {
+  IsBoolean,
+  IsDate,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateInteractionDto {
+  @IsInt()
+  @Min(0)
+  @IsNotEmpty()
+  inter_obj_i: number;
+
+  @IsInt()
+  @Min(0)
+  @IsNotEmpty()
+  inter_obj_j: number;
+
+  @IsDate()
+  @Type(() => Date)
+  @IsNotEmpty()
+  inter_start: Date;
+
+  @IsDate()
+  @Type(() => Date)
+  @IsOptional() // caso inter_end possa ser nulo inicialmente
+  inter_end: Date;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  inter_feedback: boolean;
+
+  @IsInt()
+  @Min(0)
+  @IsNotEmpty()
+  inter_service: number;
+}
