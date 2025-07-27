005b5d3197091299d9ad4f02451cf48b62c8764b
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-07-27 14:48:26 -0300

    feat: saving new pagerank friendship

--- src/pagerank-friendship/pagerank-friendship.module.ts
+++ src/pagerank-friendship/pagerank-friendship.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PagerankFriendshipService } from './pagerank-friendship.service';
 import { PagerankFriendshipController } from './pagerank-friendship.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  PageRankFriendship,
+  PageRankFriendshipSchema,
+} from './schema/pagerank-friendship.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: PageRankFriendship.name, schema: PageRankFriendshipSchema },
+    ]),
+  ],
   controllers: [PagerankFriendshipController],
   providers: [PagerankFriendshipService],
 })
--- src/pagerank-friendship/pagerank-friendship.service.ts
+++ src/pagerank-friendship/pagerank-friendship.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePagerankFriendshipDto } from './dto/create-pagerank-friendship.dto';
 import { UpdatePagerankFriendshipDto } from './dto/update-pagerank-friendship.dto';
+import { PageRankFriendship } from './schema/pagerank-friendship.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PagerankFriendshipService {
+  constructor(
+    @InjectModel(PageRankFriendship.name)
+    private pagerankFriendshipModel: Model<PageRankFriendship>,
+  ) {}
+
   create(createPagerankFriendshipDto: CreatePagerankFriendshipDto) {
-    console.log(createPagerankFriendshipDto);
-    return 'This action adds a new pagerankFriendship';
+    try {
+      const pagerankFriendship = new this.pagerankFriendshipModel(
+        createPagerankFriendshipDto,
+      );
+      const savedPagerankFriendship = pagerankFriendship.save();
+      return savedPagerankFriendship;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(
+          `Failed to create pagerankFriendship: ${error.message}`,
+        );
+      }
+      throw new Error(
+        'Failed to create pagerankFriendship due to an unknown error',
+      );
+    }
   }
 
   findAll() {
