cfee1b1468d577d39c11c2981ce8490fc87123ce
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-07-27 14:04:46 -0300

    feat: creating pagerank dto

--- src/pagerank-friendship/dto/create-pagerank-friendship.dto.ts
+++ src/pagerank-friendship/dto/create-pagerank-friendship.dto.ts
@@ -1,1 +1,9 @@
-export class CreatePagerankFriendshipDto {}
+import { IsArray, IsString } from 'class-validator';
+
+export class CreatePagerankFriendshipDto {
+  @IsString()
+  rank_object: string;
+
+  @IsArray()
+  rank_adjacency: string[];
+}
