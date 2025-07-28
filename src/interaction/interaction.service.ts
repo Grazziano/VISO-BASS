5b870f0f1a5c91c336b712f13cd899cb5ab4397b
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-07-28 10:57:49 -0300

    feat: saving new interaction

--- src/interaction/interaction.service.ts
+++ src/interaction/interaction.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateInteractionDto } from './dto/create-interaction.dto';
 import { UpdateInteractionDto } from './dto/update-interaction.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Interaction } from './schema/interaction.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class InteractionService {
+  constructor(
+    @InjectModel(Interaction.name)
+    private interactionModel: Model<Interaction>,
+  ) {}
+
   create(createInteractionDto: CreateInteractionDto) {
-    console.log(createInteractionDto);
-    return 'This action adds a new interaction';
+    try {
+      const interaction = new this.interactionModel(createInteractionDto);
+      const savedInteraction = interaction.save();
+      return savedInteraction;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`Failed to create interaction: ${error.message}`);
+      }
+      throw new Error('Failed to create interaction due to an unknown error');
+    }
   }
 
   findAll() {
