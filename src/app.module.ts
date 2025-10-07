bc9ea0cbcf22f13ea98f54f088706c75ac12e4ef
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-10-07 13:48:51 -0300

    feat: configuring ThrottlerModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,30 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { VisoObjectModule } from './viso-object/viso-object.module';
 import { AuthModule } from './auth/auth.module';
 import { OwnersModule } from './owners/owners.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), // Carrega os variáveis de ambiente do .env
     MongooseModule.forRoot(process.env.MONGO_URI!),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000, // 1 segundo
+        limit: 3, // 3 requests por segundo
+      },
+      {
+        name: 'medium',
+        ttl: 10000, // 10 segundos
+        limit: 20, // 20 requests por 10 segundos
+      },
+      {
+        name: 'long',
+        ttl: 60000, // 1 minuto
+        limit: 100, // 100 requests por minuto
+      },
+    ]),
     InteractionModule,
     OnaEnvironmentModule,
     PagerankFriendshipModule,
@@ -24,6 +43,12 @@ import { OwnersModule } from './owners/owners.module';
     OwnersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
