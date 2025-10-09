ccaa462965c135f713d86aebacdc7a68ec7ecc66
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-10-09 08:50:36 -0300

    fix: correcting the file path because it was giving an error in the tests

--- src/ona-environment/schema/ona-enviroment.schema.ts
+++ src/ona-environment/schema/ona-enviroment.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-import { VisoObject } from 'src/viso-object/schema/viso-object.schema';
+import { VisoObject } from '../../viso-object/schema/viso-object.schema';
 
 @Schema({ timestamps: true })
 export class OnaEnvironment extends Document {
--- src/viso-object/schema/viso-object.schema.ts
+++ src/viso-object/schema/viso-object.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-import { OwnersModule } from 'src/owners/owners.module';
+import { OwnersModule } from '../../owners/owners.module';
 
 export type VisoObjectDocument = VisoObject & Document;
 
